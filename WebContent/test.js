ce6e51da30e8c12e5ee735ec41b199b5095784b2
tarun <tgarg6343@gmail.com>
2018-02-27 22:13:19 +0530

    server settings changed

--- WebContent/test.js
+++ WebContent/test.js
@@ -1,17 +1,12 @@
 function getData(){
-	
-   var xhttp = new XMLHttpRequest();
-   var link = "https://developers.zomato.com/api/v2.1/search?apikey=81230e201ba9041b31fe0ad8471dd7d2&entity_id=1&entity_type=city&q=Delhi&start=0&count=5";
-   
-   xhttp.onreadystatechange = function() {
-       if (this.readyState == 4 && this.status == 200) {
-    	   
-    	  
-         var x=xhttp.responseText;
-         document.getElementById("result").innerHTML = xhttp.responseText;
-         document.write(xhttp.responseText); 
-      }
-   };
-   xhttp.open("GET", link, true);
-   xhttp.send();
+	var url = "https://developers.zomato.com/api/v2.1/search?apikey=81230e201ba9041b31fe0ad8471dd7d2&entity_id=1&entity_type=city&q=Delhi&start=0&count=5";
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	       document.getElementById("result").innerHTML = xhttp.responseText;
+	    }
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
 }
